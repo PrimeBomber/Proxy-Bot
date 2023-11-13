e910dea6f7c181a796f74d77ebc320f1118aa3b0
LinuxEnthusiast <148498389+PrimeBomber@users.noreply.github.com>
2023-11-13 18:49:35 +0100

    Update Proxy-src.js

--- Proxy-src.js
+++ Proxy-src.js
@@ -4,6 +4,7 @@ const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 
 // Initialize Telegram Bot
 const bot = new TelegramBot(token, { polling: true });
+const validStates = ["AL", "AK", "AZ", "AR", "AS", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "TT", "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY"];
 
 // Initialize SQLite Database
 const db = new sqlite3.Database('proxy_bot.db', (err) => {
